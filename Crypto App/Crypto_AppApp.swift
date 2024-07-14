e155e4fb5eaaf13e42237d4d87bd0fc84bdbb554
demola234 <kolawoleoluwasegun567@gmail.com>
2024-07-14 07:41:29 +0100

    Initial Commit

---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by Ademola Kolawole on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
